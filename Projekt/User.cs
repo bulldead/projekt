abba15d98b25ffc1d4e92b2cd30be21352a92847
Kovács Balázs <bulldead2@gmail.com>
2020-05-13 14:55:39 +0200

    moving repaired

---
+++ Projekt/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    public class User
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int publicid;
+
+        public int PublicId
+        {
+            get { return publicid; }
+            set { publicid = value; }
+        }
+        private int group;
+
+        public int Group
+        {
+            get { return group; }
+            set { group = value; }
+        }
+        private int privateid;
+
+        public int PrivateId
+        {
+            get { return privateid; }
+            set { privateid = value; }
+        }
+
+
+    }
+}
